87d4e3972a6acea98939681b58f84446192fef0b
gobhilv <64418375+gobhilv@users.noreply.github.com>
2024-03-10 14:08:25 +0530

    python test job

---
+++ first_job.py
@@ -0,0 +1,17 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+#catalogid = args['catalog-id']
+print(f"This is my test job")
+job.commit()
